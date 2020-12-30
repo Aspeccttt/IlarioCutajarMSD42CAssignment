05a59d9ddd5d2a1cc94ab73abb724beb052ce649
Aspeccttt <73173345+Aspeccttt@users.noreply.github.com>
2020-12-30 22:32:06 +0100

    30/12/2020

---
+++ HomeAssignment/IlarioCutajar42C/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+
+    [SerializeField] float ScrollSpeed = 0.05f;
+
+    Material backgroundMaterial;
+
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        backgroundMaterial = GetComponent<Renderer>().material;
+
+        offSet = new Vector2(0f, ScrollSpeed);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        backgroundMaterial.mainTextureOffset += offSet * Time.deltaTime;
+
+    }
+}
---
+++ HomeAssignment/IlarioCutajar42C/Assets/Scripts/WaveConfiguration.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Wave Configuration")]
+public class WaveConfiguration : ScriptableObject
+{
+    //The Selection which will display the chosen prefab for said wave.
+    [SerializeField] GameObject enemyPrefab;
+
+    //The Selection which will play for the certain path.
+    [SerializeField] GameObject path;
+
+    //The Selection which will display the chosen amount of enemy prefabs.
+    [SerializeField] int numberOfCars = 3;
+
+    //The Selection which will give speed to the enemy prefab at said wave.
+    [SerializeField] float enemyPrefabSpeed = 2f;
+
+    public GameObject FetchEnemyPrefab()
+    {
+        return enemyPrefab;
+    }
+
+    public List<Transform> FetchWaypoints()
+    {
+        var waveWayPoints = new List<Transform>();
+
+        foreach (Transform child in path.transform)
+        {
+            waveWayPoints.Add(child);
+        }
+        return waveWayPoints;
+    }
+
+    public float FetchEnemyPrefabSpeed()
+    {
+        return enemyPrefabSpeed;
+    }
+
+    public int FetchNumberOfCarsPerWave()
+    {
+        return numberOfCars;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
